82b6c8258860368847a095a638018b793779d702
Jaap <39458532+JeepNL@users.noreply.github.com>
2021-06-28 13:38:59 +0200

    Updated with MudBlazor Components

--- News/Program.cs
+++ News/Program.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 
 namespace News
 {
@@ -18,6 +15,8 @@ namespace News
 			builder.RootComponents.Add<App>("#app");
 
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			builder.Services.AddMudServices();
 
 			await builder.Build().RunAsync();
 		}
