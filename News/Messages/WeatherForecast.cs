3a78d779267bda4c875e8d9b808c091ccfc5c17a
Jaap <39458532+JeepNL@users.noreply.github.com>
2021-07-28 10:24:11 +0200

    Added a gRPC backend

---
+++ News/Messages/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Google.Protobuf.WellKnownTypes;
+
+// needs to be the same namespace as the /Protos/weather.proto file: INTERNAL PARTIAL CLASS
+namespace ApiGrpc.Protos
+{
+    // Properties for the underlying data are generated from the .proto file
+    // This partial class just adds some extra convenience properties
+    internal partial class WeatherForecast
+    {
+        public DateTime DateTime
+        {
+            get => Date.ToDateTime();
+            set { Date = Timestamp.FromDateTime(value.ToUniversalTime()); }
+        }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
--- News/Program.cs
+++ News/Program.cs
@@ -4,6 +4,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using Microsoft.Extensions.Configuration;
 
 namespace News
 {
@@ -14,7 +17,25 @@ namespace News
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			//builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			builder.Services.AddSingleton(services =>
+			{
+				var configuration = services.GetRequiredService<IConfiguration>();
+				var backendUrl = $"https://{configuration["Settings:BackEndUrl"]}";
+
+				// Create a channel with a GrpcWebHandler that is addressed to the backend server.
+				// GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
+				// then GrpcWeb is recommended because it produces smaller messages.
+				var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+				return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions {
+					HttpHandler = httpHandler
+					//MaxReceiveMessageSize = 1 * 1024 * 1024, // 1MB
+					//MaxSendMessageSize = 1 * 1024 * 1024, // 1MB
+					//MaxRetryAttempts = 3 // ?? #TODO
+				});
+			});
 
 			builder.Services.AddMudServices();
 
