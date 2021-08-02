b742c626796da05c8e28bcedfd834905cb22b894
Jaap <39458532+JeepNL@users.noreply.github.com>
2021-08-02 22:05:21 +0200

    Added: Theme Switcher (Light/Dark)

--- News/Helpers/Utils.cs
+++ News/Helpers/Utils.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentValidation;
+using System;
 
 namespace News.Helpers
 {
-    public static class Utils
-    {
+	public static class Utils
+	{
 		public static DateTime UtcTicks2Local(long ticks)
 		{
 			DateTime dt = new DateTime(ticks).ToLocalTime();
 			return dt;
+		}
+
+		public static FluentValueValidator<string> FluentValidate(int maxLength = 20, bool required = default, string label = default, int minLength = default)
+		{
+			if (required && minLength == default)
+				minLength = 1;
+
+			string errorMessage = $"in: {minLength}, max: {maxLength} chars."; // 'm' (or 'M') for 'min' is added next
+			errorMessage = (label == default) ? $"M{errorMessage}" : $"{label} m{errorMessage}";
+
+			string requiredMessage = string.Empty;
+			if (required)
+				requiredMessage = (label == default) ? "Required" : $"{label} required";
+
+			if (!required)
+			{
+				FluentValueValidator<string> validate = new FluentValueValidator<string>(x =>
+					x.Length(minLength, maxLength).WithMessage(errorMessage));
+				return validate;
+			}
+
+			FluentValueValidator<string> validateRequired = new FluentValueValidator<string>(x =>
+				x.NotEmpty().WithMessage(requiredMessage).Length(minLength, maxLength).WithMessage(errorMessage));
+			return validateRequired;
 		}
 	}
 }
--- News/Program.cs
+++ News/Program.cs
@@ -3,6 +3,7 @@ using Grpc.Net.Client.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MudBlazor;
 using MudBlazor.Services;
 using System;
 using System.Net.Http;
@@ -17,8 +18,20 @@ namespace News
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 
-			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-			builder.Services.AddMudServices();
+			//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+			//builder.Services.AddMudServices();
+			builder.Services.AddMudServices(config =>
+			{
+				config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+				config.SnackbarConfiguration.PreventDuplicates = false;
+				config.SnackbarConfiguration.NewestOnTop = false;
+				config.SnackbarConfiguration.ShowCloseIcon = true;
+				config.SnackbarConfiguration.VisibleStateDuration = 5000;
+				config.SnackbarConfiguration.HideTransitionDuration = 250;
+				config.SnackbarConfiguration.ShowTransitionDuration = 250;
+				config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+			});
 
 			builder.Services.AddSingleton(services =>
 			{
