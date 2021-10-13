cab4e646347499dfde3c43766cec9bfff8f34a54
JeepNL <jdkramer@tutanota.com>
2021-10-13 14:50:50 +0200

    Upgraded to .NET 6 RC2 - RC TWOOOOO!

--- News/Program.cs
+++ News/Program.cs
@@ -6,74 +6,73 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor;
 using MudBlazor.Services;
+using News;
 using News.Helpers;
 using News.Services;
 
-namespace News
+//namespace News;
+//public class Program
+//{
+//	public static async Task Main(string[] args)
+//	{
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Modify HTML <head> in components: https://devblogs.microsoft.com/aspnet/asp-net-core-updates-in-net-6-preview-7/
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore(config =>
 {
-	public class Program
+	config.AddPolicy(Policies.IsAdministrator, Policies.IsAdministratorPolicy());
+	config.AddPolicy(Policies.IsModerator, Policies.IsModeratorPolicy());
+	config.AddPolicy(Policies.IsUser, Policies.IsUserPolicy());
+});
+builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices(config =>
+{
+	config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+	config.SnackbarConfiguration.PreventDuplicates = false;
+	config.SnackbarConfiguration.NewestOnTop = false;
+	config.SnackbarConfiguration.ShowCloseIcon = true;
+	config.SnackbarConfiguration.VisibleStateDuration = 5000;
+	config.SnackbarConfiguration.HideTransitionDuration = 250;
+	config.SnackbarConfiguration.ShowTransitionDuration = 250;
+	config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
+
+builder.Services.AddSingleton(services =>
+{
+	var configuration = services.GetRequiredService<IConfiguration>();
+	var backendUrl = $"https://{configuration["Settings:BackEndUrl"]}";
+
+			// Create a channel with a GrpcWebHandler that is addressed to the backend server.
+			// GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
+			// then GrpcWeb is recommended because it produces smaller messages.
+			var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+	return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions
 	{
-		public static async Task Main(string[] args)
-		{
-			var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-			// Modify HTML <head> in components: https://devblogs.microsoft.com/aspnet/asp-net-core-updates-in-net-6-preview-7/
-			builder.RootComponents.Add<HeadOutlet>("head::after");
-			builder.RootComponents.Add<App>("#app");
-
-			builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-			builder.Services.AddOptions();
-			builder.Services.AddAuthorizationCore(config =>
-			{
-				config.AddPolicy(Policies.IsAdministrator, Policies.IsAdministratorPolicy());
-				config.AddPolicy(Policies.IsModerator, Policies.IsModeratorPolicy());
-				config.AddPolicy(Policies.IsUser, Policies.IsUserPolicy());
+		HttpHandler = httpHandler
+				//MaxReceiveMessageSize = 1 * 1024 * 1024, // 1MB
+				//MaxSendMessageSize = 1 * 1024 * 1024, // 1MB
+				//MaxRetryAttempts = 3 // ?? #TODO
 			});
-			builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
-			builder.Services.AddScoped<IAuthService, AuthService>();
+});
 
-			builder.Services.AddBlazoredLocalStorage();
-			builder.Services.AddMudServices(config =>
-			{
-				config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
-				config.SnackbarConfiguration.PreventDuplicates = false;
-				config.SnackbarConfiguration.NewestOnTop = false;
-				config.SnackbarConfiguration.ShowCloseIcon = true;
-				config.SnackbarConfiguration.VisibleStateDuration = 5000;
-				config.SnackbarConfiguration.HideTransitionDuration = 250;
-				config.SnackbarConfiguration.ShowTransitionDuration = 250;
-				config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
-			});
+builder.Services.AddSingleton<ClipboardService>();
 
-			builder.Services.AddSingleton(services =>
-			{
-				var configuration = services.GetRequiredService<IConfiguration>();
-				var backendUrl = $"https://{configuration["Settings:BackEndUrl"]}";
+// Singleton is perfect for a client-side Blazor app, but if you are working with server-side Blazor,
+// you will want to register as a Scoped service so each different user receives his/her own instance
+// of the service for the duration of their session.
+// https://wellsb.com/csharp/aspnet/blazor-singleton-pass-data-between-pages
 
-				// Create a channel with a GrpcWebHandler that is addressed to the backend server.
-				// GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
-				// then GrpcWeb is recommended because it produces smaller messages.
-				var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
-				return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions
-				{
-					HttpHandler = httpHandler
-					//MaxReceiveMessageSize = 1 * 1024 * 1024, // 1MB
-					//MaxSendMessageSize = 1 * 1024 * 1024, // 1MB
-					//MaxRetryAttempts = 3 // ?? #TODO
-				});
-			});
+builder.Services.AddSingleton<UserCookieService>();
 
-			builder.Services.AddSingleton<ClipboardService>();
-
-			// Singleton is perfect for a client-side Blazor app, but if you are working with server-side Blazor,
-			// you will want to register as a Scoped service so each different user receives his/her own instance
-			// of the service for the duration of their session.
-			// https://wellsb.com/csharp/aspnet/blazor-singleton-pass-data-between-pages
-
-			builder.Services.AddSingleton<UserCookieService>();
-
-			await builder.Build().RunAsync();
-		}
-	}
-}
+await builder.Build().RunAsync();
+//	}
+//}
--- README.md
+++ README.md
@@ -4,9 +4,9 @@ Live Site: https://blazorbits.openwiki.com/
 
 [Niels Swimberghe](https://twitter.com/RealSwimburger) wrote a great [blog post](https://swimburger.net/blog/dotnet/how-to-deploy-blazor-webassembly-to-cloudflare-pages) on how to deploy your Blazor WebAssembly Static Website to [CloudFlare Pages](https://pages.cloudflare.com/).
 
-To use the latest .NET 6 RC1 SDK on Cloudflare Pages you'll need to modify the build command: 👇
+To use the latest .NET 6 RC2 SDK on Cloudflare Pages you'll need to modify the build command: 👇
 
-`wget https://download.visualstudio.microsoft.com/download/pr/4880c5a4-9c22-47a7-b298-651f1294a385/795f7828d8684059705e625b33027f89/dotnet-sdk-6.0.100-rc.1.21458.32-linux-x64.tar.gz;tar zxf dotnet-sdk-6.0.100-rc.1.21458.32-linux-x64.tar.gz;./dotnet workload install wasm-tools;./dotnet --info;./dotnet publish -c Release -o output;`
+`wget https://download.visualstudio.microsoft.com/download/pr/20283373-1d83-4879-8278-0afb7fd4035e/56f204f174743b29a656499ad0fc93c3/dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz;tar zxf dotnet-sdk-6.0.100-rc.1.21458.32-linux-x64.tar.gz;./dotnet workload install wasm-tools;./dotnet --info;./dotnet publish -c Release -o output --self-contained;`
 
 For **wasm-tools** to work you'll need to set the environment variable **PYTHON_VERSION** to **3.7** in Cloudflare Pages settings.
 
