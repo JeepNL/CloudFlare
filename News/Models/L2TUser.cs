ab055f306fa26c31dcea1dc73ac6afdf5b2df785
JeepNL <jdkramer@tutanota.com>
2021-12-04 14:39:19 +0100

    Twitter update

--- News/Models/L2TUser.cs
+++ News/Models/L2TUser.cs
@@ -7,9 +7,11 @@
 		public string Name { get; set; }
 		public string ScreenName { get; set; }
 		public string ProfileImageUrlHttps { get; set; }
+		public string ProfileImageB64 { get; set; }
 		public string Location { get; set; }
 		public string Description { get; set; }
 		public string Url { get; set; }
+		public string Website { get; set; }
 		public bool Protected { get; set; }
 		public string CreatedAt { get; set; }
 		public ulong FollowersCount { get; set; }
@@ -17,6 +19,8 @@
 		public ulong ListedCount { get; set; }
 		public ulong StatusesCount { get; set; }
 		public ulong FavouritesCount { get; set; }
+		public bool DefaultProfile { get; set; }
+		public bool DefaultProfileImage { get; set; }
 		public bool Verified { get; set; }
 		public bool Suspended { get; set; }
 		public string AccessToken { get; set; }
